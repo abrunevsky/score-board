2a22ccf9938c618e6a9333b61e83be0a77e34f25
Alexander Brunevsky <a.brunevsky@planner5d.com>
2024-05-07 14:49:27 +0300

    Install and configure php linters

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setCacheFile(__DIR__.'/var/cache/.php-cs-fixer.cache')
+    ->setFinder($finder)
+;
